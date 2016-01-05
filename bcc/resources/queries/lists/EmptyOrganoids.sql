22c1d09a808161d5ae70f66c12214cea699be8df
leyshock <leyshock@ohsu.edu>
2016-01-05 10:34:36 -0800

    misc

--- bcc/resources/queries/lists/EmptyOrganoids.sql
+++
@@ -1,6 +0,0 @@
-SELECT Organoids.PrimaryInitiation,
-Organoids.P6Initiation,
-Organoids.PrimaryTumorCells
-FROM Organoids
-WHERE Organoids.P6Initiation IS NULL
-OR Organoids.PrimaryTumorCells IS NULL
