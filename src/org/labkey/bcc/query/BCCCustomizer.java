42e4c1f9fe4c14e524b7d9358ab3524a19d8ffda
bbimber <bbimber@gmail.com>
2016-05-06 12:22:42 -0700

    extend bcc tableCustomizer

--- src/org/labkey/bcc/query/BCCCustomizer.java
+++ src/org/labkey/bcc/query/BCCCustomizer.java
@@ -1,7 +1,9 @@
 package org.labkey.bcc.query;
 
 import org.labkey.api.data.AbstractTableInfo;
+import org.labkey.api.data.ColumnInfo;
 import org.labkey.api.data.TableInfo;
+import org.labkey.api.data.WrappedColumn;
 import org.labkey.api.ldk.LDKService;
 import org.labkey.api.ldk.table.AbstractTableCustomizer;
 import org.labkey.api.query.QueryForeignKey;
@@ -21,7 +23,15 @@ public class BCCCustomizer extends AbstractTableCustomizer
         //this allows us to apply standard customization to any table configured to use this customizer
         if (ti instanceof AbstractTableInfo)
         {
-            doSharedCustomization((AbstractTableInfo) ti);
+            AbstractTableInfo ati = (AbstractTableInfo)ti;
+
+            doSharedCustomization(ati);
+
+            //now table-specific customization:
+            if (matches(ti, "study", "demographics"))
+            {
+                customizeDemographicsTable((AbstractTableInfo) ti);
+            }
         }
     }
 
@@ -54,5 +64,41 @@ public class BCCCustomizer extends AbstractTableCustomizer
             ti.getColumn("objectid").setHidden(true);
         }
 
+    }
+
+    private void customizeDemographicsTable(AbstractTableInfo ti)
+    {
+        if (ti.getColumn("somaticMutations") == null)
+        {
+            UserSchema us = getUserSchema(ti, "study");
+            ColumnInfo col17 = getWrappedIdCol(us, ti, "somaticMutations", "demographicsSomicMutations");
+            col17.setLabel("Somatic Mutation Summary");
+            col17.setDescription("Provides summaries of somatic mutation data");
+            col17.setDisplayWidth("150");
+            ti.addColumn(col17);
+        }
+    }
+
+    /**
+     * Use this to link other queries to the demographics table.  The target query must have one row per patient.
+     * This is most commonly used to link queries that calculate some type of patient-level summary of the data,
+     * such as total mutants by type.  This linkage allows one to join other columns into any table where a patientId
+     * is present.
+     */
+    private ColumnInfo getWrappedIdCol(UserSchema us, AbstractTableInfo ds, String name, String queryName)
+    {
+        return getWrappedCol(us, ds, name, queryName, "OPTR", "OPTR");
+    }
+
+    private ColumnInfo getWrappedCol(UserSchema us, AbstractTableInfo ds, String name, String queryName, String colName, String targetCol)
+    {
+
+        WrappedColumn col = new WrappedColumn(ds.getColumn(colName), name);
+        col.setReadOnly(true);
+        col.setIsUnselectable(true);
+        col.setUserEditable(false);
+        col.setFk(new QueryForeignKey(us, null, queryName, targetCol, targetCol));
+
+        return col;
     }
 }
