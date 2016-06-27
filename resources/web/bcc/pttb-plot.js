512519485716061d444758d1691a18e838f67655
Ariel Balter <ariel@arielbalter.com>
2016-06-27 15:46:03 -0700

    added comments to pttb-plot.js

--- resources/web/bcc/pttb-plot.js
+++ resources/web/bcc/pttb-plot.js
@@ -67,23 +67,27 @@ class PlotlyPlot
 
     constructor()
     {
-        this.yaxis_list = {};
-        this.xaxis_list = {};
-        this.traces_list = {};
-        this.max_axis_number;
+        this.yaxis_collection = {}; // Collection of yaxes stored by name for easier user reference
+        this.xaxis_collection = {}; // Collection of xaxes stored by name for easier user reference
+        this.traces_collection = {}; // Collection of xaxes stored by name for easier user reference
+        this.max_axis_number; // Used to number the axes, for example yaxis1, yaxis2, ...
 
-        this.traces = [];
-        this.layout = {};
+        this.traces = []; // the actual traces list passed to plotly
+        this.layout = {}; // the actual layout object passed to plotly
         this.updateLayout(
         {
-            // default properties
+            // Code needed:
+            // Initialize layout with default properties
         });
     }
 
     set divID(divID)
     {
+        // Code needed:
         // Check to see if a div with that ID exists.
-        // Either report problem, or create one
+        // Either report problem, or create the necessary
+        // div.
+
         this.divID = divID;
     }
 
@@ -91,6 +95,7 @@ class PlotlyPlot
     {
         this.layout =
         {
+            // Code needed:
             // default values
         };
 
@@ -104,24 +109,37 @@ class PlotlyPlot
     {
         var new_yaxis =
         {
+            // Code needed:
             // default properties
         };
 
+        // This is the object to be stored in the yaxis collection.
+        // It will be keyed by its name (also stored in `yaxis_name`).
+        // It is important to understand that in the plotly layout object,
+        // yaxes are referred to as yaxis, yaxis2, yaxis3, .... However,
+        // they are referred to in the trace objects by y, y2, y3, ...
+        // This tells plotly which axis to plot the trace on (for instance,
+        // the axis for CA19-9 vs the one for Weight). This is one of the
+        // VERY confusing aspects of plotly.
+        //
+        // With this system, a user can refer to and update a yaxis by user-defined
+        // name.
         var yaxis_object =
         {
-            yaxis_name: "yaxis" + this.max_yaxis_number,
-            yaxis_name_ref: "y" + this.max_yaxis_number,
-            yaxis_properties: new_yaxis
+            yaxis_name: "yaxis" + this.max_yaxis_number, // Reference key for easy access by user
+            yaxis_name_ref: "y" + this.max_yaxis_number, // The yaxis reference put into a trace object
+            yaxis_number: max_yaxis_number,
+            yaxis_properties: new_yaxis // The yaxis name put in the layout
         }
 
-        this.max_yaxis_number += 1;
+        this.max_yaxis_number += 1; // Increment to number the next yaxis.
 
         for (var key in properties)
         {
             new_yaxis[key] = properties[key];
         }
 
-        this.yaxis_list[axis_name] = yaxis_object;
+        this.yaxis_collection[axis_name] = yaxis_object;
 
     }
 
@@ -129,10 +147,20 @@ class PlotlyPlot
     {
         for (var key in properties)
         {
-            this.yaxis_list[axis_name][key] = properties[key];
+            this.yaxis_collection[axis_name][key] = properties[key];
         }
     }
 
+    // This is the object to be stored in the yaxis collection.
+    // It will be keyed by its name (also stored in `xaxis_name`).
+    // It is important to understand that in the plotly layout object,
+    // xaxes are referred to as xaxis, xaxis2, xaxis3, .... However,
+    // they are referred to in the trace objects by x, x2, x3, ...
+    // This tells plotly which axis to plot the trace on. This is one of the
+    // VERY confusing aspects of plotly.
+    //
+    // With this system, a user can refer to and update a xaxis by user-defined
+    // name.
     addX_Axis(axis_name, properties={})
     {
         var new_xaxis =
@@ -145,21 +173,17 @@ class PlotlyPlot
             new_xaxis[key] = properties[key];
         }
 
-        for (var key in properties)
-        {
-            new_xaxis[key] = properties[key];
-        }
-
         var xaxis_object =
         {
             xaxis_name: "xaxis" + this.max_xaxis_number,
             xaxis_name_ref: "x" + this.max_xaxis_number,
+            xaxis_number: max_xaxis_number,
             xaxis_properties: new_xaxis
         }
 
         this.max_xaxis_number += 1;
 
-        this.xaxis_list[axis_name] = xaxis_object;
+        this.xaxis_collection[axis_name] = xaxis_object;
 
     }
 
@@ -167,22 +191,29 @@ class PlotlyPlot
     {
         for (var key in properties)
         {
-            this.xaxis_list[axis_name][key] = properties[key];
+            this.xaxis_collection[axis_name][key] = properties[key];
         }
     }
 
+    // I have mixed feelings about this API regarding how many parameters should be passed in.
+    // Really, everything but the name could be put in properties. My thought was to make
+    // arguments for all the truly essential non-default items. But maybe that is overkill.
     addTrace
         (
             name, // A name for the trace to allow easy removal or reconfiguration
             x, // x values
             y, // y values
-            yaxis, // Name of the associated Y-axis, e.g. "TumorSizes" or "Weight"
+            yaxisname, // Name of the associated Y-axis, e.g. "TumorSizes" or "Weight"
             title, // The name as will appear in the legend.
-            properties={} // non-default properties such as marker, line, color, hover and click annotations
+            text, // Text available to show on hover
+            hoverinfo, // what information is displayed on hover
+            properties={} // non-default properties such as marker, line, color
         )
     {
 
-        var yaxis_exists = Object.keys(yaxis_list).indexOf(yaxis) >= 0;
+        // The trace MUST be associated with a particular y axis or things
+        // will go very wrong. So this is an important check.
+        var yaxis_exists = Object.keys(yaxis_collection).indexOf(yaxis) >= 0;
         if (!yaxis_exists)
         {
             console.log("Invalid y-axis " + yaxis + ".");
@@ -196,6 +227,9 @@ class PlotlyPlot
             x: x,
             y: y,
             title: title,
+            text: text,
+            hoverinfo: hoverinfo
+            // Need code;
             // other default properties
         };
 
@@ -204,51 +238,85 @@ class PlotlyPlot
             new_trace[key] = properties[key];
         }
 
-        new_trace.yaxis = this.yaxis_list[yaxis].yaxis_name_ref;
+        // The user gives the name reference to the yaxis. However, plotly
+        // needs a value like "y", "y2", "y3", etc. So the yaxis_name_ref
+        // is extracted from the yaxis object for that name.
+        new_trace.yaxis = this.yaxis_collection[yaxis].yaxis_name_ref;
 
-        this.traces_list[name] = new_trace;
-    }
+        this.traces_collection[name] = new_trace;
+        
 
     updateTrace(name, properties={})
     {
         for (var key in properties)
         {
-            this.traces_list[name][key] = properties[key];
+            this.traces_collection[name][key] = properties[key];
         }
     }
 
     addAnnotations(schema, click=true, hover=true)
     {
-
+        // this is going ot be complicated. It will involve
+        // starting with Plotly.on('click',.....) as in current code.
+        // Hopefully we can streamline the closure issue. I think
+        // we might even want to look at the plotly source code
+        // to better understand how the click events work and what
+        // additional parameters can be passed in.
     }
 
     generateAnnotation(point)
     {
-
+        // This is basically what happens inside the click event as
+        // currently written. The click handler passes a point 
+        // object to the click callback. The callback generates
+        // an annotation and adds it to the plot.
+        //
+        // The tricky part is that we want to annotation to include
+        // more data than what comes in with the point object. I'm 
+        // not sure how isolated the click callback scope is, but 
+        // we can experiment on finding the best way to handle it.
+        // 
+        // Another point is that the way I currently have the code,
+        // I use the same function to create the text for the click
+        // popup as for the hover. So it's good if it is an independent
+        // method that gets passed into the click handler, rather
+        // than something hard-coded into the click handler.
     }
 
     generatePlot()
     {
-        xaxis_list.forEach(function(object, name)
+        // The layout has a key/value pair for each of the
+        // xaxes which are named xaxis, xaxis1, xaxis2 etc.
+        xaxis_collection.forEach(function(object, name)
         {
             var axis_name = object.xaxis_name;
             var properties = object.xaxis_properties;
             this.layout[axis_name] = properties;
         });
-
-        yaxis_list.forEach(function(object, name)
+        
+        // The layout has a key/value pair for each of the
+        // yaxes which are named yaxis, yaxis1, yaxis2 etc.
+        yaxis_collection.forEach(function(object, name)
         {
             var axis_name = object.yaxis_name;
             var properties = object.yaxis_properties;
             this.layout[axis_name] = properties;
         });
 
-        traces_list.forEach(function(object, name)
+        // We have created a traces object with key/value
+        // pairs for each trace object keyed by a user-defined
+        // name. Plotly wants a list of trace objects. So
+        // loop through the traces and push to a list.
+        traces_collection.forEach(function(object, name)
         {
             this.traces.push(object.trace_properties);
         });
 
         Plotly.newPlot(this.divID, this.traces, this.layout);
+        
+        //Plotly.onClick(...)
+        
+        //Plotly.onHover(...)
     }
 
 
