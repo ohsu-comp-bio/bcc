9daca80836a3d41a30262412d23cf25588e5b9d6
cpelz741 <pelzc@ohsu.edu>
2016-11-20 01:58:24 -0800

    PT-103 add workaround for insert as well as update.

--- resources/queries/study/Demographics.js
+++ resources/queries/study/Demographics.js
@@ -2,7 +2,7 @@
 // to auto fill the date field.
 // I am concerned that this is not the correct way to add my own script.
 //require("bcc/triggers").initScript(this);
-//var console = require("console");
+var console = require("console");
 
 function beforeInsert(row, errors) {
 	//console.log("beforeInsert got triggered");
@@ -10,6 +10,21 @@ function beforeInsert(row, errors) {
 	// We are hiding the date field, but it is required, so we will just fill
 	// it in with the current date.
 	row.date = new Date();
+    if(row['Date Of Birth']){
+        // On Insert, we are getting this format: 941-09-14 23:00:00.0
+        // Note that dates are being entered without any time.  Time is defaulting to 12 am,
+        // but in some cases the timezone is being applied incorrectly so that an hour is lost.
+        // This results in a date for the previous day.  As a workaround for this, we are going
+        // to add 12 hours to the time.  This should place us in the correct day even when a
+        // time zone calculation subtracts an hour.
+        var str = new String(row['Date Of Birth']);
+        var dobDTM = new Date(parseInt(str.substring(0,4), 10),
+                              parseInt(str.substring(5,7), 10) - 1,
+                              parseInt(str.substring(8,10), 10),
+                              parseInt(str.substring(11,13), 10) + 12, // Add 12 hours to make it the middle of the day.
+                              0,0,0);
+        row['Date Of Birth'] = dobDTM;
+    }
 }
 
 function beforeUpdate(row, oldRow, errors) {
@@ -23,11 +38,15 @@ function beforeUpdate(row, oldRow, errors) {
     }
 
     if(row['Date Of Birth']){
-
+        // On Update, we are getting this format: Tue Oct 14 23:00:00 PST 1941
+        // Note that dates are being entered without any time.  Time is defaulting to 12 am,
+        // but in some cases the timezone is being applied incorrectly so that an hour is lost.
+        // This results in a date for the previous day.  As a workaround for this, we are going
+        // to add 12 hours to the time.  This should place us in the correct day even when a
+        // time zone calculation subtracts an hour.
         var dobDTM = new Date(row['Date Of Birth']);
+        dobDTM.setHours(12); // Add 12 hours to make it the middle of the day.
         var gmtDtmStr = dobDTM.toISOString();
-        var gmtDtStr = gmtDtmStr.substring(0,10);
-
-        row['Date Of Birth'] = gmtDtStr;
+        row['Date Of Birth'] = gmtDtmStr;
     }
 }
