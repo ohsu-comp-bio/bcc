33bb20fee904627ea30a450cbde934dbe20cb557
cpelz741 <pelzc@ohsu.edu>
2016-11-21 10:52:40 -0800

    PT-103 correction based on review by JF.  Make sure that the time stamp for DOB in DB is 00:00:00.

--- resources/queries/study/Demographics.js
+++ resources/queries/study/Demographics.js
@@ -2,7 +2,7 @@
 // to auto fill the date field.
 // I am concerned that this is not the correct way to add my own script.
 //require("bcc/triggers").initScript(this);
-var console = require("console");
+//var console = require("console");
 
 function beforeInsert(row, errors) {
 	//console.log("beforeInsert got triggered");
@@ -11,7 +11,8 @@ function beforeInsert(row, errors) {
 	// it in with the current date.
 	row.date = new Date();
     if(row['Date Of Birth']){
-        // On Insert, we are getting this format: 941-09-14 23:00:00.0
+	    //console.log("beforeInsert row['Date Of Birth'] = " + row['Date Of Birth']);
+        // On Insert, we are getting this format: 1941-09-14 23:00:00.0
         // Note that dates are being entered without any time.  Time is defaulting to 12 am,
         // but in some cases the timezone is being applied incorrectly so that an hour is lost.
         // This results in a date for the previous day.  As a workaround for this, we are going
@@ -23,7 +24,10 @@ function beforeInsert(row, errors) {
                               parseInt(str.substring(8,10), 10),
                               parseInt(str.substring(11,13), 10) + 12, // Add 12 hours to make it the middle of the day.
                               0,0,0);
-        row['Date Of Birth'] = dobDTM;
+        var gmtDtmStr = dobDTM.toISOString();
+        //console.log("beforeInsert gmtDtmStr = " + gmtDtmStr);
+        // Per JF, make sure we are not adding any time information to the DB.
+        row['Date Of Birth'] = gmtDtmStr.substring(0,10);
     }
 }
 
@@ -38,6 +42,7 @@ function beforeUpdate(row, oldRow, errors) {
     }
 
     if(row['Date Of Birth']){
+	    //console.log("beforeUpdate row['Date Of Birth'] = " + row['Date Of Birth']);
         // On Update, we are getting this format: Tue Oct 14 23:00:00 PST 1941
         // Note that dates are being entered without any time.  Time is defaulting to 12 am,
         // but in some cases the timezone is being applied incorrectly so that an hour is lost.
@@ -47,6 +52,8 @@ function beforeUpdate(row, oldRow, errors) {
         var dobDTM = new Date(row['Date Of Birth']);
         dobDTM.setHours(12); // Add 12 hours to make it the middle of the day.
         var gmtDtmStr = dobDTM.toISOString();
-        row['Date Of Birth'] = gmtDtmStr;
+        //console.log("beforeUpdate gmtDtmStr = " + gmtDtmStr);
+        // Per JF, make sure we are not adding any time information to the DB.
+        row['Date Of Birth'] = gmtDtmStr.substring(0,10);
     }
 }
